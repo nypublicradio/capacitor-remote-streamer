dca70228986af43ecf5f61e2a05f841f75d4c30d
Josh <jgitlin@nypublicradio.org>
2024-07-31 10:38:18 -0400

    change pod name

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorRemoteStreamer',
+      name: 'nypublicradioCapacitorRemoteStreamer',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
