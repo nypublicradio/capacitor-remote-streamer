4131428cc3762596996ac022bbc5480b287c8c6f
bonomite <thomasbono.com@gmail.com>
2025-06-11 12:24:59 -0400

    reverted

--- src/web.ts
+++ src/web.ts
@@ -21,17 +21,12 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
       this.audio.pause();
     }
     this.audio = new Audio();
-    this.audio.id = "pluginAudioElement";
-    this.audio.crossOrigin = "anonymous";
-    this.setupEventListeners();
+    this.audio.id = "pluginAudioElement"; // Assigning an ID to the audio element
+    this.setupEventListeners(); // Call setupEventListeners here
 
     const urlWithoutParams = options.url.split('?')[0];
     if (Hls.isSupported() && urlWithoutParams.endsWith('.m3u8')) {
-      this.hls = new Hls({
-        xhrSetup: (xhr: XMLHttpRequest) => {
-          xhr.withCredentials = false;
-        }
-      });
+      this.hls = new Hls();
       this.hls.loadSource(options.url);
       this.hls.attachMedia(this.audio);
       this.hls.on(Hls.Events.MANIFEST_PARSED, async () => {
