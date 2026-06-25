50ce1aeb49f8c65906d94e9e72eda42826f13386
Thomas Bono <thomasbono.com@gmail.com>
2026-06-25 16:58:40 -0400

    better sync with phone again

--- src/web.ts
+++ src/web.ts
@@ -1,14 +1,21 @@
-import { WebPlugin } from '@capacitor/core'
-import Hls from 'hls.js'
+import { WebPlugin } from "@capacitor/core"
+import Hls from "hls.js"
 
-import type { RemoteStreamerPlugin, CarMediaItem, PlayerState } from './definitions'
+import type { RemoteStreamerPlugin, CarMediaItem, PlayerState } from "./definitions"
 
 export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin {
-  private audio: HTMLAudioElement | null = null;
-  private intervalId: number | null = null;
-  private hls: Hls | null = null;
+  private audio: HTMLAudioElement | null = null
+  private intervalId: number | null = null
+  private hls: Hls | null = null
 
-  async setNowPlayingInfo (options: { title: string; artist: string; album: string; duration: string; imageUrl: string; isLiveStream: boolean }): Promise<void> {
+  async setNowPlayingInfo (options: {
+    title: string
+    artist: string
+    album: string
+    duration: string
+    imageUrl: string
+    isLiveStream: boolean
+  }): Promise<void> {
     console.log("Setting now playing info", options)
   }
 
@@ -24,8 +31,8 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
     this.audio.id = "pluginAudioElement" // Assigning an ID to the audio element
     this.setupEventListeners() // Call setupEventListeners here
 
-    const urlWithoutParams = options.url.split('?')[0]
-    if (Hls.isSupported() && urlWithoutParams.endsWith('.m3u8')) {
+    const urlWithoutParams = options.url.split("?")[0]
+    if (Hls.isSupported() && urlWithoutParams.endsWith(".m3u8")) {
       this.hls = new Hls()
       this.hls.loadSource(options.url)
       this.hls.attachMedia(this.audio)
@@ -33,7 +40,7 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
         if (this.audio) {
           await this.audio.play()
         }
-        this.notifyListeners('play', {})
+        this.notifyListeners("play", {})
         this.startTimeUpdates()
         if (this.hls) {
           this.setupID3Listeners(this.hls)
@@ -42,7 +49,7 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
     } else {
       this.audio.src = options.url
       await this.audio.play()
-      this.notifyListeners('play', {})
+      this.notifyListeners("play", {})
       this.startTimeUpdates()
     }
   }
@@ -50,14 +57,14 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
   async pause (): Promise<void> {
     if (this.audio) {
       this.audio.pause()
-      this.notifyListeners('pause', {})
+      this.notifyListeners("pause", {})
     }
   }
 
   async resume (): Promise<void> {
     if (this.audio) {
       await this.audio.play()
-      this.notifyListeners('play', {})
+      this.notifyListeners("play", {})
     }
   }
 
@@ -76,7 +83,7 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
       this.audio.onplaying = null
       this.audio.onpause = null
       this.audio.onended = null
-      this.audio.src = ''
+      this.audio.src = ""
       this.audio.load()
       this.audio.currentTime = 0
       this.audio = null
@@ -84,9 +91,9 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
         this.hls.destroy()
         this.hls = null
       }
-      this.notifyListeners('stop', {})
+      this.notifyListeners("stop", {})
       this.stopTimeUpdates()
-      console.log('stopped', this.audio)
+      console.log("stopped", this.audio)
     }
   }
 
@@ -100,7 +107,7 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
     this.stopTimeUpdates()
     this.intervalId = window.setInterval(() => {
       if (this.audio) {
-        this.notifyListeners('timeUpdate', { currentTime: this.audio.currentTime })
+        this.notifyListeners("timeUpdate", { currentTime: this.audio.currentTime })
       }
     }, 1000)
   }
@@ -140,20 +147,22 @@ export class RemoteStreamerWeb extends WebPlugin implements RemoteStreamerPlugin
   private setupID3Listeners (hls: Hls) {
     if (hls) {
       hls.on(Hls.Events.FRAG_PARSING_METADATA, (_, data) => {
-        this.notifyListeners('id3Metadata', data)
+        this.notifyListeners("id3Metadata", data)
       })
     }
   }
 
   private setupEventListeners () {
     if (this.audio) {
-      this.audio.onplaying = () => this.notifyListeners('play', {})
-      this.audio.onpause = () => this.notifyListeners('pause', {})
-      this.audio.onended = () => this.notifyListeners('stop', { ended: true })
-      this.audio.onerror = (e) => this.notifyListeners('error', { message: `Audio error: ${e}` })
-      this.audio.onwaiting = () => this.notifyListeners('buffering', { isBuffering: true })
-      this.audio.oncanplaythrough = () => this.notifyListeners('buffering', { isBuffering: false })
-
+      this.audio.onplaying = () => this.notifyListeners("play", {})
+      this.audio.onpause = () => this.notifyListeners("pause", {})
+      this.audio.onended = () => this.notifyListeners("stop", { ended: true })
+      this.audio.onerror = (e) =>
+        this.notifyListeners("error", { message: `Audio error: ${e}` })
+      this.audio.onwaiting = () =>
+        this.notifyListeners("buffering", { isBuffering: true })
+      this.audio.oncanplaythrough = () =>
+        this.notifyListeners("buffering", { isBuffering: false })
     }
   }
 }
