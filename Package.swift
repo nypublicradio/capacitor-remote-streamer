bdd32faef71e62293d521ef1804f2322d497df45
Thomas Bono <thomasbono.com@gmail.com>
2026-09-09 01:31:38 -0400

    to v8

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "Mp3HlsStreaming",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Mp3HlsStreaming",
             targets: ["RemoteStreamerPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
